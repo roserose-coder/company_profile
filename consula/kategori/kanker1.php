19ca4718a0bcf2e1d606b0e5febb1e00f1ae2db0
Arnold1401 <Arnold.pramudita.wewe@gmail.com>
2020-09-09 18:02:18 +0700

    kanker

--- consula/kategori/kanker1.php
+++ consula/kategori/kanker1.php
@@ -1,33 +1,156 @@
-
 <?php
-  require_once "toppart.php";
+require_once "toppart.php";
 ?>
 
 <!---START ISI-->
 
-    <section class="site-section" id="blog-section">
-      <div class="container">
-        <div class="row mb-5">
-          <div class="col-12 text-center">
-            <a href="produk.php"><h3 class="section-sub-title">Produk</h3></a>
-            <h2 class="section-title mb-3">Kanker</h2>
+<section class="site-section" id="blog-section">
+  <div class="container">
+    <div class="row mb-5">
+      <div class="col-12 text-center">
+        <a href="produk.php">
+          <h3 class="section-sub-title">Produk</h3>
+        </a>
+        <h2 class="section-title mb-3">Kanker</h2>
+      </div>
+    </div>
+
+
+    <div class="row">
+
+      <div class="col-md-6">
+        <div class="card">
+          <img class="card-img-top" src="https://bantjietong.com/storeimage/obt-img/JPEG_Typhonium%20Plus%20_001.jpg" alt="Card image cap">
+          <div class="card-body">
+            <h5 class="card-title">Typhonium Plus </h5>
+            <p class="card-text text-center">Typhonium flagelliforme
+              Secara traditional digunakan pada penderita kanker / tumor
+              <br>
+              Perhatian:
+              - Dua hari pertama minum kapsul akan mual, sedikit diare, kotoran jadi hitam dan badan lesu.
+              - Kadang-kadang pasien muntah atau mual sesudah lama minum kapsul, hentikan dulu sampai gejala hilang dan di minum lagi dengan mengurangi dosis atau pemakaian sesuai petunjuk dokter.
+              - Wanita hamil tidak dianjurkan mengkonsumsi kapsul ini
+              <br>
+              Cara Pemakaian :
+              2-3 X sehari, 2 Kapsul sesudah makan ,atau menurut petunjuk dokter
+              <br>
+              BPOM:
+              TR 043330391
+            </p>
+          </div>
+          <div class="card-body">
+            <blockquote class="blockquote mb-0">
+              <footer class="blockquote-footer">#kanker#typhoniumplus#obatkanker</footer>
+            </blockquote>
+          </div>
+          <div class="card-body">
+            <a href="https://wa.me/6287764908637?text=Saya%20ingin%20pesan%20(No:58|Typhonium Plus)" target="_blank" class="card-link">Whatsapp*</a>
+            <a href="https://www.tokopedia.com/bantjietong/typhonium-plus" class="card-link">Tokopedia**</a>
           </div>
         </div>
+      </div>
 
-        https://ecs7.tokopedia.net/img/cache/200-square/hDjmkQ/2020/9/8/21a4f074-4943-4101-ae91-ec1d63c79943.jpg.webp
+      <div class="col-md-6">
 
-        <?php
-          require_once "nodataproduk.php";
-        ?>
+        <div class="card">
+          <img class="card-img-top" src="https://bantjietong.com/storeimage/obt-img/JPEG_Dragonrear%20Zhong%20Liu%20Xiao%20Ai_001.jpg" alt="Card image cap">
 
+          <div class="card-body">
+            <h5 class="card-title">Dragonrear Zhong Liu Xiao Ai</h5>
+            <p class="card-text">
+              Khasiat & Kegunaan :
+              - Membantu memelihara kesehatan pada Penderita Kanker<br>
+              - Membantu mengurangi penyebaran sel kanker / tumor<br>
+              - Membantu meningkatkan sistem kekebalan tubuh<br>
+              - Terbuat dari bahan herbal alami<br>
+              Cara Pakai (Dosis) :
+              Sehari 3 kali 8 Pil
+              <br>
+              Kemasan (Isi) :
+              1 kotak isi 120 Pil.
+              <br>
+              BPOM :
+              ti044411871
+              <br>
+              Perhatian :
+              Bila terjadi efek yang tidak diinginkan sebaiknya penggunaan produk ini harus segera dihentikan/hubungi dokter.
+            </p>
+          </div>
+
+          <div class="card-body">
+            <blockquote class="blockquote mb-0">
+              <footer class="blockquote-footer">#kanker#dragonrearzhongliuxiaoai#dragonrear</footer>
+            </blockquote>
+          </div>
+
+          <div class="card-body">
+            <a href="https://wa.me/6287764908637?text=Saya%20ingin%20pesan%20(No:40|Dragonrear Zhong Liu Xiao Ai)" target="_blank" class="card-link">Whatsapp*</a>
+            <a href="https://www.tokopedia.com/bantjietong/dragonrear-zhong-liu-xiao-ai" class="card-link">Tokopedia**</a>
+          </div>
+
+        </div>
 
       </div>
-    </section>
 
-   
-   
+
+
+    </div>
+
+
+    <div class="row">
+
+      <div class="col-md-6">
+        <div class="card">
+          <img class="card-img-top" src="https://bantjietong.com/storeimage/obt-img/JPEG_Kunir%20Putih_001.jpg" alt="Card image cap">
+          <div class="card-body">
+            <h5 class="card-title">Kunir Putih</h5>
+            <p class="card-text text-center">
+              Khasiat & Kegunaan :
+              Immunodulator (meningkatkan sistem kekebalan tubuh). Secara tradisional digunakan pada penderita kanker.
+              <br>
+              Cara Pakai (Dosis) :
+              3 x sehari 2 kapsul sampai sembuh
+              <br>
+              Kemasan (Isi) :
+              30 kapsul @500 mg
+              <br>
+              Komposisi :
+              Dalam bentuk ekstrak Curcumae Manggae, Rhizoma Extract 500 mg
+              <br>
+              BPOM :
+              tr022316511
+              <br>
+              Perhatian :
+              Simpan di tempat kering dan terhindar dari sinar matahari langsung
+            </p>
+          </div>
+          <div class="card-body">
+            <blockquote class="blockquote mb-0">
+              <footer class="blockquote-footer">#kanker#typhoniumplus#obatkanker</footer>
+            </blockquote>
+          </div>
+          <div class="card-body">
+            <a href="https://wa.me/6287764908637?text=Saya%20ingin%20pesan%20(No:167|Kunir Putih)" target="_blank" class="card-link">Whatsapp*</a>
+            <a href="https://www.tokopedia.com/bantjietong/typhonium-plus" class="card-link">Tokopedia**</a>
+          </div>
+        </div>
+      </div>
+
+
+
+
+
+    </div>
+
+
+
+  </div>
+</section>
+
+
+
 <!---END ISI-->
 
 <?php
-  require_once "bottompart.php";
+require_once "bottompart.php";
 ?>
