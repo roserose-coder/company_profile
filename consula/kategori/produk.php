fd6153434027540d56655b86813eeaf4adb51b5d
Arnold1401 <Arnold.pramudita.wewe@gmail.com>
2020-09-09 15:00:17 +0700

    produk

---
+++ consula/kategori/produk.php
@@ -0,0 +1,104 @@
+<?php
+require_once "toppart.php";
+?>
+<!-- START ISI -->
+
+<section class="site-section" id="blog-section">
+
+</section>
+
+
+<section class="site-section" id="blog-section">
+  <div class="container">
+    <div class="row mb-5">
+      <div class="col-12 text-center">
+        <h3 class="section-sub-title">Produk</h3>
+        <h2 class="section-title mb-3">Beberapa Produk</h2>
+      </div>
+    </div>
+
+    <div class="row mb-3">
+      <strong>Kategori :</strong> 
+      <a href="kanker1.php" class="border pl-1 pr-1 mr-1 ml-1 mb-1 mt-1 rounded text-dark">Kanker</a>
+      <a href="alkes.php" class="border pl-1 pr-1 mr-1 ml-1 mb-1 mt-1 rounded text-dark">Alat Kesehatan</a>
+      <a href="balita.php" class="border pl-1 pr-1 mr-1 ml-1 mb-1 mt-1 rounded text-dark">Balita</a>
+      <a href="batukpilek.php" class="border pl-1 pr-1 mr-1 ml-1 mb-1 mt-1 rounded text-dark">Batuk dan Pilek</a>
+      <a href=".php" class="border pl-1 pr-1 mr-1 ml-1 mb-1 mt-1 rounded text-dark">Ginjal</a>
+      <a href=".php" class="border pl-1 pr-1 mr-1 ml-1 mb-1 mt-1 rounded text-dark">Jantung</a>
+      <a href=".php" class="border pl-1 pr-1 mr-1 ml-1 mb-1 mt-1 rounded text-dark">Otak dan Syaraf</a>
+      <a href=".php" class="border pl-1 pr-1 mr-1 ml-1 mb-1 mt-1 rounded text-dark">Paru-paru</a>
+      <a href=".php" class="border pl-1 pr-1 mr-1 ml-1 mb-1 mt-1 rounded text-dark">Fungsi Hati</a>
+      <a href=".php" class="border pl-1 pr-1 mr-1 ml-1 mb-1 mt-1 rounded text-dark">Pria(Prostat,Kesuburan..)</a>
+      <a href=".php" class="border pl-1 pr-1 mr-1 ml-1 mb-1 mt-1 rounded text-dark">Wanita(Menstruasi,Keseuburan..)</a>
+      <a href=".php" class="border pl-1 pr-1 mr-1 ml-1 mb-1 mt-1 rounded text-dark">Saluran Air Kecil</a>
+      <a href=".php" class="border pl-1 pr-1 mr-1 ml-1 mb-1 mt-1 rounded text-dark">Mata</a>
+      <a href=".php" class="border pl-1 pr-1 mr-1 ml-1 mb-1 mt-1 rounded text-dark">Telinga</a>
+      <a href=".php" class="border pl-1 pr-1 mr-1 ml-1 mb-1 mt-1 rounded text-dark">Kulit</a>
+      <a href=".php" class="border pl-1 pr-1 mr-1 ml-1 mb-1 mt-1 rounded text-dark">Pencernaan</a>
+      <a href=".php" class="border pl-1 pr-1 mr-1 ml-1 mb-1 mt-1 rounded text-dark">Persendian</a>
+      <a href=".php" class="border pl-1 pr-1 mr-1 ml-1 mb-1 mt-1 rounded text-dark">Tulang</a>
+      <a href=".php" class="border pl-1 pr-1 mr-1 ml-1 mb-1 mt-1 rounded text-dark">Penyakit Lainnya</a>
+      <a href=".php" class="border pl-1 pr-1 mr-1 ml-1 mb-1 mt-1 rounded text-dark">Memelihara Kesehatan</a>
+      <a href=".php" class="border pl-1 pr-1 mr-1 ml-1 mb-1 mt-1 rounded text-dark">Obat Luar,Minyak,Salep</a>
+
+    </div>
+
+    <div class="row">
+
+      <div class="col-md-6">
+        <div class="card">
+          <img class="card-img-top" src="../images/covid19.jpg" alt="Card image cap">
+          <div class="card-body">
+            <h5 class="card-title">Obat Pencegahan Covid-19</h5>
+            <p class="card-text text-center">Meningkatkan daya tahan tubuh, meredakan gangguan sakit lambung, mempercepat penyembuhan luka pada penyakit diabetes serta menghilangkan bau pada luka gangren, meringankan sakit pada persendian, menghentikan pendarahan pada wasir, mempercepat penyembuhan pasca operasi maupun bersalin, menurunkan kadar kolesterol dan trigliserida, meringankan penyakit asma, memperbaiki tekstur kulit
+            </p>
+          </div>
+          <div class="card-body">
+            <blockquote class="blockquote mb-0">
+              <footer class="blockquote-footer">#covid19 #corona #covit #covid</footer>
+            </blockquote>
+          </div>
+          <div class="card-body">
+            <a href="https://wa.me/6287764908637?text=Saya%20ingin%20pesan%20(Covid-19)" target="_blank" class="card-link">Whatsapp*</a>
+            <a href="https://www.tokopedia.com/bantjietong/etalase/ramuan" class="card-link">Tokopedia**</a>
+          </div>
+        </div>
+      </div>
+
+      <div class="col-md-6">
+
+        <div class="card">
+          <img class="card-img-top" src="../images/fertility.jpg" alt="Card image cap">
+
+          <div class="card-body">
+            <h5 class="card-title">Paket Kesuburan</h5>
+            <p class="card-text">1 paket terdapat 3 bungkus ramuan untuk pria dan 3 bungkus ramuan untuk wanita</p>
+          </div>
+
+          <div class="card-body">
+            <blockquote class="blockquote mb-0">
+              <footer class="blockquote-footer">#cepathamil #kesuburan #penyubur laki #penyubur perempuan</footer>
+            </blockquote>
+          </div>
+
+          <div class="card-body">
+            <a href="https://wa.me/6287764908637?text=Saya%20ingin%20pesan%20(Paket Kesuburan)" target="_blank" class="card-link">Whatsapp*</a>
+            <a href="https://www.tokopedia.com/bantjietong/etalase/ramuan" class="card-link">Tokopedia**</a>
+          </div>
+
+        </div>
+
+      </div>
+
+
+
+    </div>
+
+  </div>
+</section>
+
+<!---END ISI-->
+
+<?php
+require_once "bottompart.php";
+?>
