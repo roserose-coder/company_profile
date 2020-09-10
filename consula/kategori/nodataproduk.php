c22dd070735e13f835daff97f60c26c3de065982
Arnold1401 <Arnold.pramudita.wewe@gmail.com>
2020-09-10 11:20:54 +0700

    nodataproduk.php

--- consula/kategori/nodataproduk.php
+++ consula/kategori/nodataproduk.php
@@ -1,4 +1,12 @@
 <div class="row">
-    <h1>Barang belum dimasukan ! Masih dalam proses .Silahkan lihat alternatf di Tokopedia kami</h1>
-    <a href="https://www.tokopedia.com/bantjietong">Cek Toko kami di Tokopedia</a>
+    <h1>Barang belum dimasukan ! Masih dalam proses .Silahkan lihat alternatif di Tokopedia kami</h1>
+    <h3><a href="https://www.tokopedia.com/bantjietong">Cek Toko kami di Tokopedia</a> <br></h3>
+  
+</div>
+<div class="row">
+<p><h5>atau</h5><br></p>
+</div>
+
+<div class="row">
+<h3> <a href="https://wa.me/6287764908637?text=Saya%20ingin%20tanya%20obat">Tanya lewat whatsapp</a></h3>
 </div>
